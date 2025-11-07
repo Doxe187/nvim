df485ed9937c79223299b231071a66242e1f2feb
Janis Gerber <janis.gerber@swisscom.com>
2025-11-07 14:07:12 +0100

    Install more parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,15 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "python", "javascript", "typescript" },
+      ensure_installed = {
+        "lua", "vim", "vimdoc",
+        "python", "javascript", "typescript", "tsx", "jsx",
+        "html", "css", "json", "yaml", "toml",
+        "bash", "sh",
+        "c", "cpp", "rust", "go",
+        "markdown", "markdown_inline",
+        "regex", "comment"
+      },
       auto_install = true,
       highlight = {
         enable = true,
