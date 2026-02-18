d6724c74707231b8945631e80f5eea4dab1990a1
Janis Gerber <janis.gerber@swisscom.com>
2026-02-18 15:38:54 +0100

    Add .md renderer

---
+++ lua/plugins/render_markdown.lua
@@ -0,0 +1,15 @@
+-- ~/.config/nvim/lua/plugins/render_markdown.lua
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",   -- Pflicht
+      "nvim-mini/mini.nvim",               -- Optional, wenn du mini-icons etc. nutzt
+      -- oder stattdessen:
+      -- "nvim-mini/mini.icons",
+      -- "nvim-tree/nvim-web-devicons"
+    },
+    opts = {}, -- hier kannst du Optionen anpassen
+  },
+}
+
